0fc4639a04365bf79c3e7a8774ef67ed942f0f86
marcelodaher <marcelodaher@live.com>
2015-08-28 00:33:28 -0300

    V0.1

---
+++ calculatorQueue.py
@@ -0,0 +1,29 @@
+class calculatorQueue:
+    """A LIFO Queue to keep number data"""
+    q = [] #where we will keep our numbers
+    
+    def __init__(self):
+        self.q = []
+    
+    def __str__(self):
+        """A method to print the queue"""
+        s = "Queue:"
+        for element in self.q:
+            s += '\n' + str(element)
+        return s
+    
+    def push(self, number):
+        """Method to add elements to the top of the queue"""
+        self.q.append(number)
+    
+    def get (self):
+        """Method to pop and return first element"""
+        return self.q.pop();
+    
+    def clear(self):
+        """Method to empty the queue"""
+        q.clear()
+    
+    def empty(self):
+        return 0 == len(self.q)
+    
